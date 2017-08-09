320eb342a4294ed7c3b0181c22e979a5eb1991b1
Shuzhen Wang <shuzhenwang@google.com>
2017-08-09 13:39:47 -0700

    wayne-common: QCamera2: HAL3: Fix missing pthread unlock
    Test: Camera CTS
    Bug: 36716469
    Change-Id: I6699c3682eec419fb6efb0583e9909e5739476a0
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- QCamera2/HAL3/QCamera3HWI.cpp
+++ QCamera2/HAL3/QCamera3HWI.cpp
@@ -8633,6 +8633,7 @@ no_error:
         if (channel == NULL) {
             LOGE("invalid channel pointer for stream");
             assert(0);
+            pthread_mutex_unlock(&mMutex);
             return BAD_VALUE;
         }
 
@@ -8710,6 +8711,7 @@ no_error:
         } else {
             LOGE("Internal requests not supported on this stream type");
             assert(0);
+            pthread_mutex_unlock(&mMutex);
             return INVALID_OPERATION;
         }
         latestRequest->internalRequestList.push_back(requestedStream);
