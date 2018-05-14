c624709ef91743a60e78daf6f9f5b4a3af53a0f9
Michael Bestas <mkbestas@lineageos.org>
2018-05-14 22:15:24 +0300

    jasmine_sprout: Don't build tinymix
    * This tool can be built manually for debugging cases,
      there is no need to ship it in production

--- device.mk
+++ device.mk
@@ -123,8 +123,7 @@ PRODUCT_PACKAGES += \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
-    libvolumelistener \
-    tinymix
+    libvolumelistener
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl:32 \
