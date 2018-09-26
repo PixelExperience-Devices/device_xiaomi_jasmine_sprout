589e0d31a3bfe6dac81baa19ede7b3388559c54a
Bruno Martins <bgcngm@gmail.com>
2018-09-26 09:54:39 +0100

    jasmine_sprout: Move offmode charging daemon away
     * Follow-up change for commit dae4bf86 that simply moves the
       prebuilt to its proper place.
    
    Change-Id: I40d37ea6ae53fe8fe919dda758db22df3c1c4229

--- device.mk
+++ device.mk
@@ -247,9 +247,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-service
 
-PRODUCT_PACKAGES += \
-    chargeonlymode
-
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0
