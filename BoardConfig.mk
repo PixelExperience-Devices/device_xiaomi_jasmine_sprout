16d7510b0c469556d1253281b67b5679e3f6f848
srfarias <erikofarias@gmail.com>
2019-08-10 01:46:36 -0300

    jasmine_sprout:: boardconfig: disable dexpreopt for prebuilts
    Signed-off-by: srfarias <erikofarias@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -255,6 +255,7 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
+      DONT_DEXPREOPT_PREBUILTS := true
       WITH_DEXPREOPT := true
       WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
     endif
