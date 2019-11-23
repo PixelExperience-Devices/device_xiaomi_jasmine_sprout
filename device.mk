2d53c8fcc9891b402788e3032bc79c27982740cf
Mikhail Naganov <mnaganov@google.com>
2019-11-22 17:34:45 -0800

    jasmine_sprout: Switch to Audio HAL V6
    Switch jasmine_sprout to Audio HAL V6
    
    Test: VtsHalAudioV6_0TargetTest VtsHalAudioEffectV6_0TargetTest
    Bug: 155591007
    (cherry picked from commit f11963392c9afd79704822a5812ef4412f3810db)

--- device.mk
+++ device.mk
@@ -119,9 +119,9 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@5.0-impl \
+    android.hardware.audio@6.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
