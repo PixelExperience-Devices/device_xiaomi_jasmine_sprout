f685366c0823d2a8c42d3560d3585a551926b528
Arjun Sreenath <asreen@codeaurora.org>
2020-10-16 00:56:55 +0530

    QCamera2: Initialize mAllocathread variable
    Issue : While running cts getting invalid pthread passed to pthread_join.
    
    Fix : In few instances mAllocathread used to take random values.
    Due to this invalid pthread is passed for pthread_join.
    
    Change-Id: I11b525438ff5c0a3776f39d6e313a47187f85caa

--- QCamera2/HAL3/QCamera3Channel.cpp
+++ QCamera2/HAL3/QCamera3Channel.cpp
@@ -903,6 +903,7 @@ QCamera3ProcessingChannel::QCamera3ProcessingChannel(uint32_t cam_handle,
     property_get("persist.vendor.debug.sf.showfps", prop, "0");
     mDebugFPS = (uint8_t) atoi(prop);
     mReqFrameNumList.clear();
+    mAllocThread = 0;
 
     int32_t rc = m_postprocessor.init(&mMemory);
     if (rc != 0) {
