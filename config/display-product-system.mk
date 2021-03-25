ccddb825d9d70316a3f90ffd7207292ff90632cc
Pullakavi Srinivas <spullaka@codeaurora.org>
2021-03-25 15:00:50 +0530

    commonsys: Enable unified draw.
    CRs-Fixed: 2908286
    Change-Id: Ieb73ecc55b155b9035d6276153cf7e12b662d5d7

--- config/display-product-system.mk
+++ config/display-product-system.mk
@@ -18,10 +18,13 @@ PRODUCT_PACKAGES += libdisplayconfig \
                     vendor.qti.hardware.display.config-V2-ndk_platform
 
 SOONG_CONFIG_NAMESPACES += qtidisplaycommonsys
+SOONG_CONFIG_NAMESPACES += qtiunifeddraw
 # Soong Keys
 SOONG_CONFIG_qtidisplaycommonsys := displayconfig_enabled
+SOONG_CONFIG_qtiunifeddraw := qtiunifeddraw_enabled
 # Soong Values
 SOONG_CONFIG_qtidisplaycommonsys_displayconfig_enabled := false
+SOONG_CONFIG_qtiunifeddraw_qtiunifeddraw_enabled := true
 
 ifeq ($(call is-vendor-board-platform,QCOM),true)
     SOONG_CONFIG_qtidisplaycommonsys_displayconfig_enabled := true
