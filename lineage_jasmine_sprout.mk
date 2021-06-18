1cec38ca3959b30f477de831c42ad1db60f010a9
aswin7469 <aswinas@pixysos.com>
2021-06-18 15:18:20 +0530

    jasmine_sprout:Use roms auto generated builf fingerprint and description

--- lineage_jasmine_sprout.mk
+++ lineage_jasmine_sprout.mk
@@ -34,8 +34,3 @@ PRODUCT_NAME := lineage_jasmine_sprout
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 TARGET_VENDOR_PRODUCT_NAME := jasmine
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="jasmine-user 8.1.0 OPM1.171019.011 V9.6.17.0.ODIMIFE release-keys"
-
-BUILD_FINGERPRINT := xiaomi/jasmine/jasmine_sprout:8.1.0/OPM1.171019.011/V9.6.17.0.ODIMIFE:user/release-keys
