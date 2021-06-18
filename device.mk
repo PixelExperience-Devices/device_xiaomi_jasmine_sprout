761f03032bdecec5460f438adbe8429af0285bd4
yincen17 <52388255+yincen17@users.noreply.github.com>
2021-06-18 15:12:31 +0530

    jasmine_sprout:Remove lineage livedisplay

--- device.mk
+++ device.mk
@@ -339,10 +339,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.jasmine_sprout
 
-# LiveDisplay native
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
