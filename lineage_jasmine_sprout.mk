b6b2071bda3a36c86a3980e76b0256f07df9ce68
Ray <42294756+rayrzy@users.noreply.github.com>
2021-06-19 10:45:31 +0530

    jasmine_sprout:Add TARGET_BOOT_ANIMATION_RES

--- lineage_jasmine_sprout.mk
+++ lineage_jasmine_sprout.mk
@@ -22,6 +22,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Boot Aniamation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Inherit from jasmine_sprout device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
