347386c96073542dcbad2b3ab6c977164478d5dc
StyloGey <stylogey@gmail.com>
2021-07-17 12:06:07 +0200

    camera: guard lib2d rotation with QCPATH

--- QCamera2/stack/mm-jpeg-interface/Android.mk
+++ QCamera2/stack/mm-jpeg-interface/Android.mk
@@ -25,7 +25,11 @@ endif
 LOCAL_C_INCLUDES+= $(kernel_includes)
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 
+ifneq ($(QCPATH),)
+LIB2D_ROTATION=true
+else
 LIB2D_ROTATION=false
+endif
 
 LOCAL_C_INCLUDES += \
     $(LOCAL_PATH)/inc \
