a666e81baf7b0322081247ff75e5418fe8bbd68d
OdSazib <odsazib@gmail.com>
2021-07-18 16:41:11 +0600

    camera: QCamera2: filter out lib2d-interface

--- QCamera2/stack/Android.mk
+++ QCamera2/stack/Android.mk
@@ -1,5 +1,7 @@
 LOCAL_PATH:= $(call my-dir)
 include $(LOCAL_PATH)/mm-camera-interface/Android.mk
 include $(LOCAL_PATH)/mm-jpeg-interface/Android.mk
+ifneq ($(QCPATH),)
 include $(LOCAL_PATH)/mm-lib2d-interface/Android.mk
+endif
 include $(LOCAL_PATH)/common/leak/Android.mk
