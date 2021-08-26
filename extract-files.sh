b40b98b4bb62588c51f42feba397940d2176e261
Edwin Moquete <edwinmmoquete@gmail.com>
2021-08-26 16:51:49 +0530

    jasmine_sprout: Update sha1 sum of camera.sdm660.so
    Remove android.hidl.base@1.0 dependency.

--- extract-files.sh
+++ extract-files.sh
@@ -62,6 +62,10 @@ function blob_fixup() {
         "${PATCHELF}" --remove-needed "vendor.xiaomi.hardware.mtdservice@1.0.so" "${2}"
         sed -i "s|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g" "${2}"
         ;;
+
+    vendor/lib/hw/camera.sdm660.so)
+        "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
+        ;;
     esac
 }
 
