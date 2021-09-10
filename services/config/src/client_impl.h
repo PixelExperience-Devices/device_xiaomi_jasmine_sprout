4b85308a71de9dc4c6ff31926b2dc99fccb97da0
Ray Zhang <rayz@codeaurora.org>
2021-09-10 17:51:44 +0800

    commonsys: services: upgrade IDC 2.0 for backward compatible
    The IDC libs in vendor and system images are not compatible due
    to diverged changes. E.g., in legacy vendor kGetDisplayTileCount
    op code is 50, while in new system images op code 50 stands for
    kDummyOpCode. This breaks the backward compatibility. So upgrade
    IDC lib in new system to fix the backward compatible issues for
    vendor freeze targets.
    
    Change-Id: Ia7552d2f06db04db149fd7fe3bf875c1406421c8

--- services/config/src/client_impl.h
+++ services/config/src/client_impl.h
@@ -27,6 +27,42 @@
 * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+/*
+ * Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the
+ * disclaimer below) provided that the following conditions are met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *    * Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *
+ *    * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #ifndef __CLIENT_IMPL_H__
 #define __CLIENT_IMPL_H__
 
@@ -65,6 +101,7 @@ class ClientCallback: public IDisplayConfigCallback {
   void ParseNotifyCWBBufferDone(const ByteStream &input_params, const HandleStream &input_handles);
   void ParseNotifyQsyncChange(const ByteStream &input_params);
   void ParseNotifyIdleStatus(const ByteStream &input_params);
+  void ParseNotifyCameraSmooth(const ByteStream &input_params);
   ConfigCallback *callback_ = nullptr;
 };
 
@@ -89,6 +126,7 @@ class ClientImpl : public ClientInterface {
   virtual int SetIdleTimeout(uint32_t value);
   virtual int GetHDRCapabilities(DisplayType dpy, HDRCapsParams *caps);
   virtual int SetCameraLaunchStatus(uint32_t on);
+  virtual int SetCameraSmoothInfo(CameraSmoothOp op, uint32_t fps);
   virtual int DisplayBWTransactionPending(bool *status);
   virtual int SetDisplayAnimating(uint64_t display_id, bool animating);
   virtual int ControlIdlePowerCollapse(bool enable, bool synchronous);
@@ -127,6 +165,14 @@ class ClientImpl : public ClientInterface {
   virtual int IsSupportedConfigSwitch(uint32_t disp_id, uint32_t config, bool *supported);
   virtual int GetDisplayType(uint64_t physical_disp_id, DisplayType *disp_type);
   virtual int AllowIdleFallback();
+  virtual int GetDisplayTileCount(uint64_t physical_disp_id, uint32_t *num_h_tiles,
+                                  uint32_t *num_v_tiles);
+  virtual int SetPowerModeTiled(uint64_t physical_disp_id, PowerMode power_mode,
+                                uint32_t tile_h_loc, uint32_t tile_v_loc);
+  virtual int SetPanelBrightnessTiled(uint64_t physical_disp_id, uint32_t level,
+                                      uint32_t tile_h_loc, uint32_t tile_v_loc);
+  virtual int SetWiderModePreference(uint64_t physical_disp_id, WiderModePref mode_pref);
+  virtual int ControlCameraSmoothCallback(bool enable);
   virtual int DummyDisplayConfigAPI();
 
  private:
