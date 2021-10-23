682b0f3dbf96ef0d5f2b877a938f37a45e42f52e
SakilMondal <mondalsakil@gmail.com>
2021-10-23 21:06:02 +0530

    jasmine_sprout: Drop ifaa bootjar

--- device.mk
+++ device.mk
@@ -160,9 +160,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     org.ifaa.android.manager
 
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     BluetoothQti \
