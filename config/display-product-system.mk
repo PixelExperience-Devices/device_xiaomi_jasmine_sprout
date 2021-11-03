b6bea6ef4bf22a521b7a17de1ac3822230b9f5f0
Ramakant Singh <ramakant@codeaurora.org>
2021-11-03 15:10:58 +0530

    aidl: add callback interface to notify fps mitigation.
    Change-Id: I71a310bc645a901f85fdceab259b3b25aea354ee
    CRs-Fixed: 3049689

--- config/display-product-system.mk
+++ config/display-product-system.mk
@@ -8,7 +8,8 @@ PRODUCT_PACKAGES += vendor.display.config@1.0 \
                     vendor.qti.hardware.display.config-V2-ndk \
                     vendor.qti.hardware.display.config-V3-ndk \
                     vendor.qti.hardware.display.config-V4-ndk \
-                    vendor.qti.hardware.display.config-V5-ndk
+                    vendor.qti.hardware.display.config-V5-ndk \
+                    vendor.qti.hardware.display.config-V6-ndk
 
 SOONG_CONFIG_NAMESPACES += qtiunifeddraw
 # Soong Keys
