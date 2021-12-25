e90c49c962786dd28044b855b5176638c28bba6d
Edwin Wong <edwinwong@google.com>
2021-12-25 07:35:16 +0530

    jasmine_sprout: Update clearkey plugin to drm HAL v1.4 Test: atest   atest CtsMediaTestCases:android.media.cts.MediaDrmClearkeyTest#testGetPlaybackComponent
    Bug: 182525516
    Change-Id: I406b7d3c2921d4698535421894c6eb3785eb1c0d

--- device.mk
+++ device.mk
@@ -220,8 +220,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3-service.clearkey \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.4.vendor
 
 # Framework detect
 PRODUCT_PACKAGES += \
