6dbb0a1619bf671b6b1f7b20d9f963524e2b7120
Ethan Chen <intervigil@gmail.com>
2022-01-11 09:35:44 +0530

    jasmine_sprout: Remove unnecessary netutils packages * These are already packaged by AOSP.
    Change-Id: I77f91fbbb33a6d891d14fe852b094ab914ca3111

--- device.mk
+++ device.mk
@@ -347,8 +347,7 @@ PRODUCT_COPY_FILES += \
 
 # Net
 PRODUCT_PACKAGES += \
-    android.system.net.netd@1.1.vendor \
-    netutils-wrapper-1.0
+    android.system.net.netd@1.1.vendor
 
 # OMX
 PRODUCT_PACKAGES += \
