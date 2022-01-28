11aa371245347e7a637a8dad9bb5db5d6b0fe2d1
iamimmanuelraj <immanuelr44@gmail.com>
2022-01-28 23:49:17 +0530

    jasmine_sprout: Update radio hal and radio configs hal to latest
    To Add this https://www.eff.org/deeplinks/2022/01/victory-google-releases-disable-2g-feature-new-android-smartphones#:~:text=If%20you%20have%20an%20older%20Android%20phone%2C%20these%20steps%20may%20or%20may%20not%20work.%20Unfortunately%20due%20to%20limitations%20of%20old%20hardware%2C%20Google%20was%20only%20able%20to%20implement%20this%20feature%20on%20phones%20running%20Android%2012%20and%20supporting%20version%201.6%20of%20the%20radio%20HAL%2C%20so%20far%20this%20is%20limited%20to%20the%20Pixel%206.
    
    A major flaw.. We use old blobs still so lets update the vintf later when doing blobs

--- device.mk
+++ device.mk
@@ -401,8 +401,8 @@ PRODUCT_COPY_FILES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.5.vendor \
-    android.hardware.radio.config@1.2.vendor \
+    android.hardware.radio@1.6.vendor \
+    android.hardware.radio.config@1.3.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.2.vendor \
     libavservices_minijail.vendor \
