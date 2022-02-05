4ea44c1d6922ea92b427176cec2bf839b93c69b8
orges <me@orgesified.ml>
2022-02-05 16:07:05 +0530

    jasmine_sprout: Add display config file  * DisplayPowerController[0]: Screen brightness nits configuration is unavailable; falling back  * DisplayDeviceConfig: requesting nits when no mapping exists.
    The max nits supported by Mi A2 as per GSM Arena is 420 nits
    Min is 10 cause its the default value that reocvery uses and also its the value before the screen goes dark/blank . We can go lower but still safetry is better

--- device.mk
+++ device.mk
@@ -212,6 +212,10 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     vendor.display.config@1.1
 
+# Display Config
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/displayconfig/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
+
 # Doze
 PRODUCT_PACKAGES += \
     XiaomiDoze
