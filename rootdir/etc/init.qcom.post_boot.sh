ae7fdd01e29e89c7058d4c79d423abc8020aacb9
iamimmanuelraj <immanuelr44@gmail.com>
2022-02-08 00:58:04 +0530

    jasmine_sprout: rootdir: Change background cpuset

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -2918,7 +2918,7 @@ case "$target" in
             echo 120 > /proc/sys/kernel/sched_group_downmigrate
 
             # cpuset settings
-            echo 0-3 > /dev/cpuset/background/cpus
+            echo 0-1 > /dev/cpuset/background/cpus
             echo 0-3 > /dev/cpuset/system-background/cpus
 
             #if the kernel version >=4.14,use the schedutil governor
