927a79f88b4cdba8186786a09d66a3a862336e26
iamimmanuelraj <immanuelr44@gmail.com>
2022-02-22 22:20:53 +0530

    Revert "jamsine_sprout: Switch to bootcontrol 1.2"
    This reverts commit 55d127afd0735ffbdc890d4dfeee24d8c38fcbd6.

--- device.mk
+++ device.mk
@@ -83,9 +83,9 @@ PRODUCT_PACKAGES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-impl.recovery \
-    android.hardware.boot@1.2-service \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-impl.recovery \
+    android.hardware.boot@1.0-service \
     bootctrl.sdm660 \
     bootctrl.sdm660.recovery
 
