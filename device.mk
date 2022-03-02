f086b2c908dc1acaf18973f9bf98e91468832d71
Alex Hong <rurumihong@google.com>
2022-03-02 17:11:12 +0530

    jasmine_sprout: Replace the HIDL with AIDL for "android.hardware.memtr… …ack"
    The HIDL interface of "android.hardware.memtrack" is no longer supported for Android S.
    
    Test: atest VtsHalMemtrackTargetTest pass
    Bug: 186539435
    Change-Id: Iaafd526fc5a601f7e8189ec764399d99c660beb0

--- device.mk
+++ device.mk
@@ -219,8 +219,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.graphics.composer@2.1-impl:64 \
     android.hardware.graphics.composer@2.1-service \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
+    android.hardware.memtrack-service.example \
     vendor.display.config@1.1
 
 # Display Config
