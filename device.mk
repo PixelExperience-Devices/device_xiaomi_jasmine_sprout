93db9208028475faee5e03e841e431a8ec935fcd
iamimmanuelraj <immanuelr44@gmail.com>
2022-03-02 22:49:51 +0530

    jasmine_sprout: Add Build thermal hal
    03-02 17:38:24.278  1372  1372 E HardwarePropertiesManagerService-JNI: Unable to get Thermal service.

--- device.mk
+++ device.mk
@@ -464,6 +464,9 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0.vendor
+
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
