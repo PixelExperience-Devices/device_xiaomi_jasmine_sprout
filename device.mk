3acc46c6f5112390dabc34990715aa3c4cf1b9ad
David Li <dvdli@google.com>
2022-03-23 23:33:25 +0800

    jasmine_sprout: Upgrade audio.usb.default from v1 to v2
    Bug: 187912470
    Test: make -j128
    Test: deep buffer and compress offload via speaker, USB headset, BT
    earphone
    Test: phone call via handset
    Test: hotword detection
    
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- device.mk
+++ device.mk
@@ -104,6 +104,7 @@ PRODUCT_PACKAGES += \
     audio.primary.sdm660 \
     audio.r_submix.default \
     audio.usb.default \
+    audio.usbv2.default \
     libaudio-resampler \
     libqcompostprocbundle \
     libqcomvisualizer \
