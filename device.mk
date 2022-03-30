4ba77eaee56938951c2a9e4709684e38cdea3de0
clarencelol <clarencekuiek@icloud.com>
2022-03-30 18:13:32 +0530

    jasmine_sprout: thermal: Simplify thermal configs * Remove all just leave battery monitor to its task
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- device.mk
+++ device.mk
@@ -475,9 +475,6 @@ PRODUCT_BOOT_JARS += \
     telephony-ext
 
 # Thermal
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
-
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0.vendor
 
