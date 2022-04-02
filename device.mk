fde14e281465d3f560ece71cc1b65c921eb9d25e
iamimmanuelraj <immanuelr44@gmail.com>
2022-04-02 21:17:25 +0530

    jasmine_sprout: Build servicetracker interfaces from source

--- device.mk
+++ device.mk
@@ -457,6 +457,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
     android.hardware.sensors@1.0-service
 
+# Servicetracker
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.servicetracker@1.2.vendor
+
 # Tethering
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
