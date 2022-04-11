2ba2b4637445bbb077744212394079bc71b5613e
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-04-11 20:49:23 +0530

    jasmine_sprout: Optimise package manager dex flags Change-Id: I70b4f33bc206f4b5e0878c78db55663460a18fd0

--- device.mk
+++ device.mk
@@ -203,6 +203,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
+# Dex Flags
+DONT_DEXPREOPT_PREBUILTS := true
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+
 # Display
 PRODUCT_PACKAGES += \
     gralloc.sdm660 \
