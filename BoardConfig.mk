a63b3c830d0de94e8330709f17f4bdada1789c79
johnmart19 <johnivan19999@gmail.com>
2022-07-09 17:35:24 +0530

    jasmine_sprout: Switch to SkiaGL Threaded - SkiaVK works, in some cases HwUI works better, but mostly shuttery and microlags, so let's go for SkiaGL Threaded
    Signed-off-by: Immanuel Raj <iamimmanuelraj@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,6 +110,7 @@ DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/manifests/compatibility_matrix.xml
 
 # HWUI
 HWUI_COMPILE_FOR_PERF := true
+USE_OPENGL_RENDERER := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1  service_locator.enable=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3 loop.max_part=7 printk.devkmsg=on kpti=off
