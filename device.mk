1c66d2d3e4bf915ff5c79f9a509e5b9033378ecd
Immanuel Raj <iamimmanuelraj@gmail.com>
2022-07-24 22:44:53 +0530

    jasmine_sprout: Switch to caf V1.1 BootCtrl Hal
    Signed-off-by: Immanuel Raj <iamimmanuelraj@gmail.com>

--- device.mk
+++ device.mk
@@ -90,9 +90,9 @@ PRODUCT_PACKAGES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-impl.recovery \
-    android.hardware.boot@1.0-service \
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery \
+    android.hardware.boot@1.1-service \
     bootctrl.sdm660 \
     bootctrl.sdm660.recovery
 
