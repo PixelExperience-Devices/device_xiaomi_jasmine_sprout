33d624abe51d475c74b47308718df7bd8cbe4df7
Immanuel Raj <iamimmanuelraj@gmail.com>
2022-07-25 00:16:29 +0530

    jasmine_sprout: Remove RRO flag
    Signed-off-by: Immanuel Raj <iamimmanuelraj@gmail.com>

--- device.mk
+++ device.mk
@@ -13,8 +13,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay
 
-PRODUCT_ENFORCE_RRO_TARGETS := *
-
 # PRODUCT_SHIPPING_API_LEVEL indicates the first api level, device has been commercially launched on.
 PRODUCT_SHIPPING_API_LEVEL := 27
 
