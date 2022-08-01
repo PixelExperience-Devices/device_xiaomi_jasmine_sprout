01fb576d50a8a285258fe0df2c2c97c6591dfd34
Immanuel Raj <iamimmanuelraj@gmail.com>
2022-08-01 19:06:40 +0530

    jasmine_sprout: Add `gpio-keys.kl`
    From caf
    
    The default generic one in aosp https://github.com/PixelExperience/frameworks_base/blob/twelve/data/keyboards/Generic.kl has some values differing from caf(stock one)
    
    Also change kl location as per aosp location https://github.com/PixelExperience/build/blob/79e4f83967325b8eb822150ddf6d4d5e44c82ec5/core/envsetup.mk#L503
    
    Signed-off-by: Immanuel Raj <iamimmanuelraj@gmail.com>

--- device.mk
+++ device.mk
@@ -343,7 +343,8 @@ PRODUCT_COPY_FILES += \
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
-    $(DEVICE_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
+    $(DEVICE_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl \
+    $(DEVICE_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
 
 # Keymaster
 PRODUCT_PACKAGES += \
