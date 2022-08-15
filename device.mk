e79459f199fd68ed1b6613ac4c1dc53117214e1d
Immanuel Raj <iamimmanuelraj@gmail.com>
2022-08-15 16:30:49 +0530

    jasmine_sprout: Build some necessary packages
    Functionality
    
    Signed-off-by: Immanuel Raj <iamimmanuelraj@gmail.com>

--- device.mk
+++ device.mk
@@ -190,6 +190,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5:64 \
+    android.hardware.camera.provider@2.6:64 \
     libdng_sdk.vendor \
     vendor.qti.hardware.camera.device@1.0:64 \
     camera.sdm660 \
@@ -225,7 +226,11 @@ PRODUCT_PACKAGES += \
     memtrack.sdm660 \
     libdisplayconfig \
     libtinyxml.vendor \
-    libtinyxml
+    libtinyxml \
+    libgralloc.qti \
+    libqdMetaData \
+    libqdMetaData.system \
+    libqdMetaData.vendor
 
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
