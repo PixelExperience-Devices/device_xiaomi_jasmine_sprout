891e02042493305e38b0e1bdeb6c17468356bed2
Josh Wu <joshwu@google.com>
2022-08-20 01:45:32 +0530

    jasmine_sprout: Switch Bt HAL to AIDL
    Signed-off-by: Immanuel Raj <iamimmanuelraj@gmail.com>

--- device.mk
+++ device.mk
@@ -136,7 +136,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth.audio@2.1-impl:32 \
+    android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor
 
