8e77cc256ba01c765094e3522d8c9a1fb452d273
Luofan Chen <clfbbn@gmail.com>
2022-09-26 18:52:16 +0530

    jasmine_sprout: Build hostapd libraries
    Since wpa-suppliant is now switched to AIDL, it no longer
    builds the HIDL interfaces that the prebuilt software binaries
    need.
    
    Build the HIDL interface to make the prebuilt executable happy.

--- device.mk
+++ device.mk
@@ -585,7 +585,8 @@ PRODUCT_PACKAGES += \
     libwifi-hal-qcom \
     libwpa_client \
     wpa_supplicant \
-    wpa_supplicant.conf
+    wpa_supplicant.conf \
+    android.hardware.wifi.hostapd@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
