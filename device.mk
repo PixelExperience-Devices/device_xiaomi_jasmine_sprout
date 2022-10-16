d6715e498a799b1b7025422fab68f709e36c35e7
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2022-10-17 00:25:00 +0530

    jasmine_sprout: Switch to QTI usb hal

--- device.mk
+++ device.mk
@@ -571,7 +571,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.3-service-qti
 
 # Vibrator
 PRODUCT_PACKAGES += \
