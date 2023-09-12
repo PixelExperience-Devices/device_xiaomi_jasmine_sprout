2657ee675bb050032fd7596d36fe002ecf698f30
Immanuel Raj <iamimmanuelraj@gmail.com>
2023-09-12 19:48:43 +0530

    jasmine_sprout: Define kernel version in device.mk
    Dk why but this is needed even after setting in bc.mk for some micros to work

--- device.mk
+++ device.mk
@@ -390,6 +390,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# Kernel
+TARGET_KERNEL_VERSION := 4.19
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
