106135ec8538cc01d71e7a7235ce6cddc7a288dd
Immanuel Raj <iamimmanuelraj@gmail.com>
2023-09-22 00:58:14 +0530

    jasmine_sprout: Add developer gsi key make file

--- aosp_jasmine_sprout.mk
+++ aosp_jasmine_sprout.mk
@@ -9,6 +9,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
 # Inherit some common Pixel Experience stuff
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
