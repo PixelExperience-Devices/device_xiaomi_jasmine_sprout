a355fb38a828125e6ca3862d585c11e78a54ba38
Immanuel Raj <iamimmanuelraj@gmail.com>
2023-11-14 20:45:25 +0100

    jasmine_sprout: Dont build tinyxml
    Ref: https://github.com/PixelExperience-Devices/device_qcom_common/commit/2ea8bf414968fa5fc042bb3703cf78c3bcd48fb6

--- device.mk
+++ device.mk
@@ -241,7 +241,6 @@ PRODUCT_PACKAGES += \
     hwcomposer.sdm660 \
     memtrack.sdm660 \
     libdisplayconfig \
-    libtinyxml \
     libgralloc.qti \
     libqdMetaData \
     libqdMetaData.system
